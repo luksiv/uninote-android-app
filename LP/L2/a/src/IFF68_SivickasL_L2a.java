abd624b11a496035d15a3656c3bba849d7b56ee9
Lukas Šivickas <l.sivickas@gmail.com>
2018-10-08 14:05:28 +0300

    LP: L2a pradetas

---
+++ LP/L2/a/src/IFF68_SivickasL_L2a.java
@@ -0,0 +1,11 @@
+/*
+Variantas - LD2a
+Autorius - Lukas Sivickas
+ */
+
+public class IFF68_SivickasL_L2a {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
