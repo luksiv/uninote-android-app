eec24b863e7e85fc436a8185463d0a9858a2d6ea
Lukas Šivickas <l.sivickas@gmail.com>
2018-11-06 17:09:53 +0200

    Add convertToString(Date date) method

--- app/src/main/java/lukas/sivickas/uninote/Tools.java
+++ app/src/main/java/lukas/sivickas/uninote/Tools.java
@@ -1,4 +1,21 @@
 package lukas.sivickas.uninote;
 
-public class tools {
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class Tools {
+    public static String convertToString(Date date) {
+        Calendar calendar = GregorianCalendar.getInstance(); // creates a new calendar instance
+        calendar.setTime(date);   // assigns calendar to given date
+        calendar.get(Calendar.HOUR_OF_DAY); // gets hour in 24h format
+        calendar.get(Calendar.HOUR);        // gets hour in 12h format
+        calendar.get(Calendar.MONTH);
+        return String.format("%d-%d-%d %d:%d",
+                calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.DAY_OF_MONTH),
+                calendar.get(Calendar.HOUR_OF_DAY),
+                calendar.get(Calendar.MINUTE));
+    }
 }
