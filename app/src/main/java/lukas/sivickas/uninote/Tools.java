810cea1fe00d8216bc63e548cacfbf93638b4ff2
Lukas Šivickas <l.sivickas@gmail.com>
2018-11-18 18:13:07 +0200

    Add zero padding to date to string conversion method

--- app/src/main/java/lukas/sivickas/uninote/Tools.java
+++ app/src/main/java/lukas/sivickas/uninote/Tools.java
@@ -8,14 +8,25 @@ public class Tools {
     public static String convertToString(Date date) {
         Calendar calendar = GregorianCalendar.getInstance(); // creates a new calendar instance
         calendar.setTime(date);   // assigns calendar to given date
-        calendar.get(Calendar.HOUR_OF_DAY); // gets hour in 24h format
-        calendar.get(Calendar.HOUR);        // gets hour in 12h format
-        calendar.get(Calendar.MONTH);
-        return String.format("%d-%d-%d %d:%d",
-                calendar.get(Calendar.YEAR),
-                calendar.get(Calendar.MONTH)+1,
-                calendar.get(Calendar.DAY_OF_MONTH),
-                calendar.get(Calendar.HOUR_OF_DAY),
-                calendar.get(Calendar.MINUTE));
+        String year = String.valueOf(calendar.get(Calendar.YEAR));
+        String month = String.valueOf(calendar.get(Calendar.MONTH)+1);
+        String day = String.valueOf(calendar.get(Calendar.DAY_OF_MONTH));
+        String hour = String.valueOf(calendar.get(Calendar.HOUR_OF_DAY));
+        String minute = String.valueOf(calendar.get(Calendar.MINUTE));
+        if(calendar.get(Calendar.MONTH)+1 < 10)
+            month = "0" + month;
+        if(calendar.get(Calendar.DAY_OF_MONTH) < 10)
+            day = "0" + day;
+        if(calendar.get(Calendar.HOUR_OF_DAY) < 10)
+            hour = "0" + hour;
+        if(calendar.get(Calendar.MINUTE) < 10)
+            minute = "0" + minute;
+
+        return String.format("%s-%s-%s %s:%s",
+                year,
+                month,
+                day,
+                hour,
+                minute);
     }
 }
